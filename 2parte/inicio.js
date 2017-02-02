1222bacec0963dc374f86f17ae29561919afe73a
Jeremias <jerepn_93@hotmail.com>
2017-02-02 12:40:07 +0000

    2parte

--- 2parte/inicio.js
+++ 2parte/inicio.js
@@ -3,9 +3,9 @@ $(document).ready(function() {
 
     ready = new elements();
 
-    var money = $('<label>Precio</label><input type="number" required value="750" id="money"><label>€</label><br>');
-    var exit = $('<label>Hora Salida</label><input type="time" value="" required id="exit"><br>');
-    var enter = $('<label>Hora Llegada</label><input type="time" value="" required id="enter"><br>');
+    var money = $('<label>Precio</label><input type="number" required:true value="750" id="money"><label>€</label><br>');
+    var exit = $('<label>Hora Salida</label><input type="time" value="00:00" required:true id="exit"><br>');
+    var enter = $('<label>Hora Llegada</label><input type="time" value="12:00" required:true id="enter"><br>');
     var selector = $('<label>Aerolineas</label><select id="aerolineas"></select><br>');
     var send = $('<input type="button" value="Buscar" id="send">');
 
